b05126c15166b39f76d01414f01745aa2d6b9346
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-04-17 13:27:47 -0700

    Add sign in page

---
+++ gro/lib/screens/authenticate/sign_in.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import '../../services.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Sign in page"),
+    );
+  }
+}
