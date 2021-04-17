f6e0fa8abe0b215b8517982f0ca13f28b8062cf7
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2021-04-17 15:54:40 -0700

    home page

--- gro/lib/screens/home.dart
+++ gro/lib/screens/home.dart
@@ -8,8 +8,133 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+<<<<<<< Updated upstream
     return Scaffold(
       body: Text("Home")
+=======
+    final mediaQuery = MediaQuery.of(context);
+
+    final pageBody = Container(
+      height: (mediaQuery.size.height - mediaQuery.padding.top),
+      width: mediaQuery.size.width,
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: mediaQuery.padding.top),
+          Container(
+            child: Text(
+              "My Plants",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 50,
+              ),
+            ),
+          ),
+          Spacer(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        SizedBox(width: 50),
+                        Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              SizedBox(width: 60),
+                              Text("Hello"),
+                              SizedBox(width: 170),
+                              Text("Hello"),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 60),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        SizedBox(width: 50),
+                        Container(
+                          height: 150,
+                          width: 150,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              SizedBox(width: 60),
+                              Text("Hello"),
+                              SizedBox(width: 170),
+                              Text("Hello"),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+          Spacer(flex: 2),
+        ],
+      ),
+    );
+
+    return Scaffold(
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: Colors.white,
+      // ),
+      body: pageBody,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        backgroundColor: Colors.green,
+      ),
+>>>>>>> Stashed changes
     );
   }
 }
