2ebf2a47775dcbc6e3e223dad95322bd51177279
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-04-18 00:06:54 -0700

    Add firebase support for home

--- gro/lib/screens/home.dart
+++ gro/lib/screens/home.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:gro/screens/owned_plants.dart';
 import 'package:gro/screens/survey/name_question.dart';
 import '../screens.dart';
+import '../services.dart';
 
 class Home extends StatefulWidget {
   static const routeName = '/home';
@@ -10,52 +13,104 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List plants = [1, 2, 3, 4];
+  String uid;
+
+  Stream plants;
+
+  fetchUserID() {
+    uid = FirebaseAuth.instance.currentUser.uid;
+  }
+
+  fetchUserPlants() async {
+    await DatabaseService().getUsersPlants(uid).then((results) {
+      setState(() {
+        plants = results;
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchUserID();
+    fetchUserPlants();
+  }
 
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
 
-    final plantList = SingleChildScrollView(
-      child: Container(
-        child: GridView.builder(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 30,
-              mainAxisSpacing: 30,
-            ),
-            itemCount: 4,
-            shrinkWrap: true,
-            itemBuilder: (BuildContext txt, index) {
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => OwnedPlant(),
-                      ));
-                },
-                child: Ink(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                          height: 120,
-                          width: 120,
-                          decoration: BoxDecoration(
-                            color: Colors.red,
-                            shape: BoxShape.circle,
-                          )),
-                      SizedBox(height: 15),
-                      Text("Plant $index"),
-                    ],
-                  ),
-                ),
+    final plantList = Container(
+      height: (mediaQuery.size.height - mediaQuery.padding.top),
+        padding: const EdgeInsets.only(bottom: 50),
+        child: StreamBuilder(
+          stream: plants,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("Error"),
               );
-            }),
-      ),
-    );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (snapshot.connectionState == ConnectionState.active) {
+              if (snapshot.data.docs.length == 0) {
+                return Center(child: Text("You do not have any plants."));
+              }
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 30,
+                  mainAxisSpacing: 30,
+                ),
+                itemCount: snapshot.data.docs.length,
+                shrinkWrap: true,
+                itemBuilder: (BuildContext txt, index) {
+
+                  Widget img;
+                  DocumentSnapshot plant = snapshot.data.docs[index];
+
+                  if (plant['image'] == "null") {
+                    img = CircleAvatar(
+                        radius: 20,
+                        backgroundColor: Colors.grey,
+                        child: Text(plant['plant_name'].substring(0, 1),
+                            style: TextStyle(color: Colors.white, fontSize: 40)));
+                  } else {
+                    img =
+                        CircleAvatar(radius: 20, backgroundImage: NetworkImage(plant['image']));
+                  }
+                  return InkWell(
+                    onTap: () {
+                      Navigator.of(context).pushNamed(OwnedPlant.routeName, arguments: plant.id);
+                    },
+                    child: Ink(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                            child: img,
+                          ),
+                          SizedBox(height: 15),
+                          Text("${plant['plant_name']}"),
+                        ],
+                      ),
+                    ),
+                  );
+                }
+              );
+            } else {
+              return Text('');
+            }
+          }
+        ),
+      );
+
 
     final pageBody = Container(
         height: (mediaQuery.size.height - mediaQuery.padding.top),
