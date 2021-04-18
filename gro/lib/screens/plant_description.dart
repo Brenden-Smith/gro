9b28135579e3e4fe76be1fdb90ea32af439ec42e
Lydia Yang <64216620+lyds214@users.noreply.github.com>
2021-04-17 19:22:15 -0700

    dfdf

---
+++ gro/lib/screens/plant_description.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PlantDesc extends StatefulWidget {
+  @override
+  _PlantDescState createState() => _PlantDescState();
+}
+
+class _PlantDescState extends State<PlantDesc> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- gro/lib/screens/plant_search.dart
+++ gro/lib/screens/plant_search.dart
@@ -1,11 +1,10 @@
 // API from https://trefle.io/
 
 import 'package:flutter/material.dart';
+import 'package:gro/screens/plant_description.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import '../models.dart';
-
-
 
 class PlantSearch extends StatefulWidget {
   @override
@@ -13,35 +12,36 @@ class PlantSearch extends StatefulWidget {
 }
 
 class _PlantSearchState extends State<PlantSearch> {
-
   Future<List<Plant>> queryResults;
 
   @override
-    void initState() {
-      super.initState();
-      queryResults = _searchPlant("rose");
-    }
+  void initState() {
+    super.initState();
+    queryResults = _searchPlant("rose");
+  }
 
   Future<List<Plant>> _searchPlant(String q) async {
-
     // If query is empty
-    if (q=="") {
+    if (q == "") {
       List<Plant> empty = [];
       return empty;
     }
 
     // API queries
     final Map<String, String> query = {
-      'token' : 'e4MKtEkxTyNFuwO3P5jQB5WIPlan2Tx0s3tKs0NFXSU',
-      'q' : q,
+      'token': 'e4MKtEkxTyNFuwO3P5jQB5WIPlan2Tx0s3tKs0NFXSU',
+      'q': q,
     };
 
     // Get info from API
-    final response = await http.get(Uri.https("trefle.io", "/api/v1/plants/search", query));
+    final response =
+        await http.get(Uri.https("trefle.io", "/api/v1/plants/search", query));
 
     // If response code is 200
     if (response.statusCode == 200) {
-      final output = new List<Plant>.from(json.decode(response.body)['data'].map((data) => Plant.fromJson(data)));
+      final output = new List<Plant>.from(json
+          .decode(response.body)['data']
+          .map((data) => Plant.fromJson(data)));
       output.sort((a, b) {
         return a.name.toLowerCase().compareTo(b.name.toLowerCase());
       });
@@ -49,37 +49,31 @@ class _PlantSearchState extends State<PlantSearch> {
     } else {
       throw Exception("Failed to search");
     }
-
-
   }
 
   @override
   Widget build(BuildContext context) {
-    
     final buildResults = Container(
-      child: FutureBuilder<List<Plant>>(
-        future: queryResults,
-        builder: (conext, snapshot) {
-          if (snapshot.hasData) {
-            return GridView.builder(
-              padding: EdgeInsets.all(30),
-              itemCount: snapshot.data.length,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 30,
-                crossAxisSpacing: 30,
-              ),
-              itemBuilder: (context, index) {
-                return PlantCard(context, snapshot.data[index]);
+        child: FutureBuilder<List<Plant>>(
+            future: queryResults,
+            builder: (conext, snapshot) {
+              if (snapshot.hasData) {
+                return GridView.builder(
+                    padding: EdgeInsets.all(30),
+                    itemCount: snapshot.data.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 30,
+                      crossAxisSpacing: 30,
+                    ),
+                    itemBuilder: (context, index) {
+                      return PlantCard(context, snapshot.data[index]);
+                    });
+              } else if (snapshot.hasError) {
+                return Text("${snapshot.error}");
               }
-            );
-          } else if (snapshot.hasError) {
-            return Text("${snapshot.error}");
-          }
-          return CircularProgressIndicator();
-        }       
-      )
-    );
+              return CircularProgressIndicator();
+            }));
 
     return Scaffold(
       appBar: AppBar(
@@ -96,67 +90,72 @@ class _PlantSearchState extends State<PlantSearch> {
   }
 
   Widget PlantCard(BuildContext context, Plant plant) {
-    
     Widget img;
 
-    if (plant.image=="null") {
-      img = CircleAvatar(radius: 20, backgroundColor: Colors.grey, child: Text(plant.name.substring(0, 1), style: TextStyle(color: Colors.white, fontSize: 40)));
+    if (plant.image == "null") {
+      img = CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.grey,
+          child: Text(plant.name.substring(0, 1),
+              style: TextStyle(color: Colors.white, fontSize: 40)));
     } else {
-      img = CircleAvatar(radius: 20, backgroundImage: NetworkImage(plant.image));
+      img =
+          CircleAvatar(radius: 20, backgroundImage: NetworkImage(plant.image));
     }
     return new Container(
-      // decoration: BoxDecoration(
-      //   shape: BoxShape.rectangle,
-      //   border: Border(
-      //     top: BorderSide(
-      //       width: 1,
-      //       color: Colors.grey[300],
-      //     ),
-      //     bottom: BorderSide(
-      //       width: 1,
-      //       color: Colors.grey[300],
-      //     ),
-      //     left: BorderSide(
-      //       width: 1,
-      //       color: Colors.grey[300],
-      //     ),
-      //     right: BorderSide(
-      //       width: 1,
-      //       color: Colors.grey[300],
-      //     ),
-      //   ),
-      //   borderRadius: BorderRadius.all(Radius.circular(10.0)),
-      //   color: Colors.white,
-      //   boxShadow: [
-      //     BoxShadow(
-      //       color: Colors.black.withOpacity(0.5),
-      //       spreadRadius: 1,
-      //       blurRadius: 3,
-      //       offset: Offset(0, 5), // changes position of shadow
-      //     ),
-      //   ],
-      // ),
-      child: InkWell(
-        onTap: () {},
-        child: Ink(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                height: 100,
-                width: 100,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-                child: img,
+        // decoration: BoxDecoration(
+        //   shape: BoxShape.rectangle,
+        //   border: Border(
+        //     top: BorderSide(
+        //       width: 1,
+        //       color: Colors.grey[300],
+        //     ),
+        //     bottom: BorderSide(
+        //       width: 1,
+        //       color: Colors.grey[300],
+        //     ),
+        //     left: BorderSide(
+        //       width: 1,
+        //       color: Colors.grey[300],
+        //     ),
+        //     right: BorderSide(
+        //       width: 1,
+        //       color: Colors.grey[300],
+        //     ),
+        //   ),
+        //   borderRadius: BorderRadius.all(Radius.circular(10.0)),
+        //   color: Colors.white,
+        //   boxShadow: [
+        //     BoxShadow(
+        //       color: Colors.black.withOpacity(0.5),
+        //       spreadRadius: 1,
+        //       blurRadius: 3,
+        //       offset: Offset(0, 5), // changes position of shadow
+        //     ),
+        //   ],
+        // ),
+        child: InkWell(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => PlantDesc()));
+            },
+            child: Ink(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: img,
+                  ),
+                  SizedBox(height: 15),
+                  Text("${plant.name}"),
+                ],
               ),
-              SizedBox(height: 15),
-              Text("${plant.name}"),
-            ],
-          ),
-        )  
-      )
-    );
+            )));
   }
 }
